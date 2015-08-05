e2364f25d81c176ebe208d029da26738a6589dfb
Gabriel Pichot <gabriel.pichot@gmail.com>
2015-08-05 18:11:00 +0200

    Use find_packages for package discovery

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open(os.path.join(os.path.dirname(__file__), 'README.markdown')) as readme:
     README = readme.read()
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 setup(
     name='django-paiji2-weather',
     version='0.1',
-    packages=['paiji2_weather'],
+    packages=find_packages(),
     include_package_data=True,
     description='A simple weather app',
     long_description=README,
