190fd1c7c9d4b95c95f5014a047dc88625ad2f6c
Gabriel Pichot <gabriel.pichot@gmail.com>
2015-08-06 15:48:36 +0200

    Added testing, codeclimate and coverage

---
+++ testsettings.py
@@ -0,0 +1,33 @@
+import django.conf.global_settings as DEFAULT_SETTINGS
+
+
+SECRET_KEY = 'tsktsktsk'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    },
+}
+
+INSTALLED_APPS = (
+    # Default Django apps
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    # We test this one
+    'paiji2_weather',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
